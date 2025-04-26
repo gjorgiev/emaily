b2a885abf32d66236467a6e9592abab6ddac7561
Georgi Georgiev <georgi.coding@proton.me>
2025-04-26 17:43:59 +0200

    7 add mongodb (#8)
    * init commit
    
    * added mongoose
    
    ---------
    
    Co-authored-by: Georgi Georgiev <georgigeorgiev@iMac-von-Georgi.fritz.box>

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(process.env.MONGO_URI);
 
 const app = express();
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile", profile);
-      console.log("done", done);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [process.env.COOKIE_KEY],
   })
 );
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
