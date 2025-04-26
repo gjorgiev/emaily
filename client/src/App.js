b570d8be9becf23f878900ae21a82137592564ee
Georgi Georgiev <georgigeorgiev@iMac-von-Georgi.fritz.box>
2025-04-26 21:04:59 +0200

    adding client side

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hi there!</p>
+        <a href="/auth/google">Sign in with google</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
